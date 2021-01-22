e2bf67f53b905db3dc94b7fd4dced8320ecfa192
anil ekici <anilekicipt@gmail.com>
2021-01-22 20:22:20 +0000

    database changes to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_01_22_192822) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string "ip"
     t.string "country"
